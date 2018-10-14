749fd9a293ba824251d40e1c32dcc9f0cde33471
Tim Robinson <tim.g.robinson@gmail.com>
2018-10-14 20:25:14 +0100

    Initial commit

---
+++ examples/wsdump.rs
@@ -0,0 +1,130 @@
+extern crate bytes;
+extern crate futures;
+extern crate tokio;
+extern crate url;
+extern crate ws_lite;
+
+#[macro_use]
+extern crate structopt;
+
+use std::io::{self, Read, Write};
+use std::result;
+use std::time::{Duration, Instant};
+
+use bytes::{Bytes, BytesMut};
+use futures::{Async, Future, IntoFuture, Sink, Stream};
+use futures::future::{self, Either, Loop};
+use structopt::StructOpt;
+use tokio::timer::Delay;
+use url::Url;
+use ws_lite::{ClientBuilder, Message, Result};
+
+struct Stdin(BytesMut);
+
+impl Stdin {
+    pub fn new() -> Self {
+        Stdin(BytesMut::new())
+    }
+}
+
+impl Stream for Stdin {
+    type Item = Bytes;
+    type Error = io::Error;
+
+    fn poll(&mut self) -> result::Result<Async<Option<Bytes>>, io::Error> {
+        let Stdin(ref mut buffer) = self;
+        buffer.reserve(4096);
+        buffer.resize(4096, 0);
+
+        let n = io::stdin().read(&mut *buffer)?;
+        buffer.truncate(n);
+
+        if n == 0 {
+            Ok(Async::Ready(None))
+        } else {
+            Ok(Async::Ready(Some(buffer.clone().freeze())))
+        }
+    }
+}
+
+fn parse_secs(s: &str) -> Result<Duration> {
+    let n = s.parse()?;
+    Ok(Duration::from_secs(n))
+}
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    /// wait time(second) after 'EOF' received.
+    #[structopt(long = "eof-wait", parse(try_from_str = "parse_secs"), default_value = "0")]
+    eof_wait: Duration,
+
+    /// websocket url. ex. ws://echo.websocket.org/
+    #[structopt(parse(try_from_str = "Url::parse"))]
+    url: Url,
+}
+
+fn main() -> Result<()> {
+    let Opt { eof_wait, url } = Opt::from_args();
+
+    let f = ClientBuilder::new(url)
+        .connect()
+        .and_then(move |client| {
+            let (sink, stream) = client.split();
+
+            let send_loop = future::loop_fn((Stdin::new(), sink), move |(stream, sink)| {
+                stream
+                    .into_future()
+                    .map_err(|(e, _stream)| Into::into(e))
+                    .and_then(move |(data, stream)| {
+                        if let Some(data) = data {
+                            Either::A(
+                                Message::new(true, data)
+                                    .map_err(Into::into)
+                                    .into_future()
+                                    .and_then(|message| sink.send(message))
+                                    .map(|sink| Loop::Continue((stream, sink))),
+                            )
+                        } else {
+                            Either::B(
+                                Delay::new(Instant::now() + eof_wait)
+                                    .map_err(Into::into)
+                                    .and_then(|()| future::ok(Loop::Break(()))),
+                            )
+                        }
+                    })
+            });
+
+            let recv_loop = future::loop_fn(stream, |stream| {
+                stream
+                    .into_future()
+                    .map_err(|(e, _stream)| e)
+                    .and_then(|(message, client)| {
+                        let message = if let Some(message) = message {
+                            message
+                        } else {
+                            return Ok(Loop::Break(()));
+                        };
+
+                        let bytes = if let Some(s) = message.as_text() {
+                            s.as_bytes()
+                        } else {
+                            &message.data()
+                        };
+
+                        let stdout = io::stdout();
+                        let mut stdout = stdout.lock();
+                        stdout.write_all(bytes)?;
+                        stdout.flush()?;
+                        Ok(Loop::Continue(client))
+                    })
+            });
+
+            Future::select(send_loop, recv_loop)
+                .map(|(value, _other)| value)
+                .map_err(|(e, _other)| e)
+        })
+        .map_err(|e| println!("{}", e));
+
+    tokio::run(f);
+    Ok(())
+}
---
+++ src/lib.rs
@@ -0,0 +1,463 @@
+extern crate base64;
+extern crate byteorder;
+extern crate bytes;
+extern crate futures;
+extern crate httparse;
+extern crate native_tls;
+extern crate rand;
+extern crate tokio_io;
+extern crate tokio_tcp;
+extern crate tokio_tls;
+extern crate url;
+
+use std::error;
+use std::io::{self, Cursor, Read};
+use std::mem;
+use std::net::ToSocketAddrs;
+use std::ops::Range;
+use std::result;
+use std::str::{self, Utf8Error};
+
+use base64::display::Base64Display;
+use bytes::{BufMut, Bytes, BytesMut};
+use byteorder::{BigEndian, ReadBytesExt};
+use futures::{Future, Stream};
+use futures::future::{self, Either, IntoFuture};
+use httparse::Response;
+use native_tls::TlsConnector;
+use tokio_io::{AsyncRead, AsyncWrite};
+use tokio_tls::TlsConnectorExt;
+use tokio_tcp::TcpStream;
+use url::Url;
+
+#[allow(deprecated)]
+use tokio_io::codec::{Decoder, Encoder, Framed};
+
+pub type Error = Box<error::Error + Sync + Send + 'static>;
+
+pub type Result<T> = result::Result<T, Error>;
+
+pub trait AsyncReadWrite: AsyncRead + AsyncWrite {}
+
+impl<S> AsyncReadWrite for S
+where
+    S: AsyncRead + AsyncWrite,
+{
+}
+
+#[derive(Clone, Debug)]
+pub struct Message {
+    is_text: bool,
+    data: Bytes,
+}
+
+impl Message {
+    pub fn new(is_text: bool, data: Bytes) -> result::Result<Self, Utf8Error> {
+        if is_text {
+            str::from_utf8(&data)?;
+        }
+
+        Ok(Message { is_text, data })
+    }
+
+    pub fn text(data: &str) -> Self {
+        Message {
+            is_text: true,
+            data: data.into(),
+        }
+    }
+
+    pub fn data(&self) -> &Bytes {
+        &self.data
+    }
+
+    pub fn as_text(&self) -> Option<&str> {
+        if self.is_text {
+            Some(unsafe { str::from_utf8_unchecked(&self.data) })
+        } else {
+            None
+        }
+    }
+}
+
+struct UpgradeCodec;
+
+impl Decoder for UpgradeCodec {
+    type Item = ();
+    type Error = Error;
+
+    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<()>> {
+        let response_len = {
+            let mut headers = [httparse::EMPTY_HEADER; 20];
+            let mut response = Response::new(&mut headers);
+            let status = response.parse(&src)?;
+            if !status.is_complete() {
+                return Ok(None);
+            }
+
+            status.unwrap()
+        };
+
+        src.advance(response_len);
+        Ok(Some(()))
+    }
+}
+
+impl Encoder for UpgradeCodec {
+    type Item = ();
+    type Error = Error;
+
+    fn encode(&mut self, _item: (), _dst: &mut BytesMut) -> Result<()> {
+        unimplemented!()
+    }
+}
+
+struct FrameHeader {
+    fin: bool,
+    opcode: u8,
+    mask: Option<[u8; 4]>,
+    len: usize,
+}
+
+macro_rules! try_eof {
+    ($result: expr) => {{
+        let result: result::Result<_, io::Error> = $result;
+        match result {
+            Ok(value) => value,
+            Err(e) => {
+                if e.kind() == io::ErrorKind::UnexpectedEof {
+                    return Ok(None);
+                } else {
+                    return Err(e.into());
+                }
+            }
+        }
+    }};
+}
+
+impl FrameHeader {
+    fn validate(data: &[u8]) -> Result<Option<(Self, Range<usize>)>> {
+        let mut c = Cursor::new(data);
+
+        let (fin, opcode) = {
+            let b = try_eof!(c.read_u8());
+
+            let fin = match b & 0xf0 {
+                0x00 => false,
+                0x80 => true,
+                _ => {
+                    return Err("reserved bits are not supported".into());
+                }
+            };
+
+            (fin, b & 0x0f)
+        };
+
+        let (mask, len) = {
+            let b = try_eof!(c.read_u8());
+
+            let len = match b & 0x7f {
+                127 => try_eof!(c.read_u64::<BigEndian>()) as usize,
+                126 => try_eof!(c.read_u16::<BigEndian>()) as usize,
+                n => {
+                    assert!(n < 126);
+                    n as usize
+                }
+            };
+
+            let mask = if b & 0x80 == 0 {
+                None
+            } else {
+                let mut mask = [0; 4];
+                try_eof!(c.read_exact(&mut mask));
+                Some(mask)
+            };
+
+            (mask, len)
+        };
+
+        let data_start = c.position() as usize;
+        let data_end = data_start + len;
+        if data.len() < data_end {
+            return Ok(None);
+        }
+
+        let header = FrameHeader {
+            fin,
+            opcode,
+            mask,
+            len,
+        };
+
+        Ok(Some((header, data_start..data_end)))
+    }
+
+    fn write_to(&self, dst: &mut BytesMut) {
+        if !self.fin {
+            assert_eq!(0, self.opcode);
+        }
+
+        dst.reserve(10 + self.len as usize);
+        dst.put_u8((if self.fin { 0x80 } else { 0x00 }) | self.opcode);
+
+        let mask_bit = if self.mask.is_some() { 0x80 } else { 0x00 };
+        if self.len > 65535 {
+            dst.put_u8(mask_bit | 127);
+            dst.put_u64_be(self.len as u64);
+        } else if self.len >= 126 {
+            dst.put_u8(mask_bit | 126);
+            dst.put_u16_be(self.len as u16);
+        } else {
+            dst.put_u8(mask_bit | self.len as u8);
+        }
+
+        if let Some(mask) = &self.mask {
+            dst.reserve(4);
+            dst.put_slice(mask);
+        }
+    }
+}
+
+pub struct MessageCodec;
+
+impl Decoder for MessageCodec {
+    type Item = Message;
+    type Error = Error;
+
+    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Message>> {
+        let (header, data_range) = if let Some(tuple) = FrameHeader::validate(&src)? {
+            tuple
+        } else {
+            return Ok(None);
+        };
+
+        assert!(header.fin);
+
+        let is_text = if header.opcode == 1 {
+            true
+        } else {
+            assert_eq!(header.opcode, 2);
+            false
+        };
+
+        let data = src.split_to(data_range.end)
+            .freeze()
+            .slice(data_range.start, data_range.end);
+
+        Ok(Some(Message::new(is_text, data)?))
+    }
+}
+
+impl Encoder for MessageCodec {
+    type Item = Message;
+    type Error = Error;
+
+    fn encode(&mut self, item: Message, dst: &mut BytesMut) -> Result<()> {
+        let mask: u32 = rand::random();
+        let mask: [u8; 4] = unsafe { mem::transmute(mask) };
+
+        let header = FrameHeader {
+            fin: true,
+            opcode: if item.is_text { 1 } else { 2 },
+            mask: Some(mask),
+            len: item.data.len(),
+        };
+
+        header.write_to(dst);
+
+        let mut data = BytesMut::from(item.data);
+        for (b, &mask) in data.iter_mut().zip(mask.iter().cycle()) {
+            *b = *b ^ mask;
+        }
+
+        dst.put(data);
+        Ok(())
+    }
+}
+
+#[allow(deprecated)]
+pub type Client<S> = Framed<S, MessageCodec>;
+
+pub struct ClientBuilder {
+    url: Url,
+    key: Option<[u8; 16]>,
+}
+
+impl ClientBuilder {
+    pub fn new(url: Url) -> Self {
+        ClientBuilder { url, key: None }
+    }
+
+    pub fn key(mut self, key: &[u8]) -> Self {
+        let mut a = [0; 16];
+        a.copy_from_slice(key);
+        self.key = Some(a);
+        self
+    }
+
+    pub fn connect(
+        self,
+    ) -> impl Future<Item = Client<Box<AsyncReadWrite + Sync + Send + 'static>>, Error = Error>
+    {
+        self.url
+            .to_socket_addrs()
+            .map_err(Into::into)
+            .and_then(|mut addrs| {
+                addrs
+                    .next()
+                    .ok_or_else(|| "can't resolve host".to_owned().into())
+            })
+            .into_future()
+            .and_then(|addr| TcpStream::connect(&addr).map_err(Into::into))
+            .and_then(move |stream| {
+                if self.url.scheme() == "wss" {
+                    Either::A(
+                        TlsConnector::builder()
+                            .and_then(|builder| builder.build())
+                            .map_err(Into::into)
+                            .into_future()
+                            .and_then(move |cx| {
+                                cx.connect_async(self.url.domain().unwrap_or(""), stream)
+                                    .map_err(Into::into)
+                                    .map(|stream| {
+                                        let b: Box<
+                                            AsyncReadWrite + Sync + Send + 'static,
+                                        > = Box::new(stream);
+                                        (b, self)
+                                    })
+                            }),
+                    )
+                } else {
+                    let b: Box<AsyncReadWrite + Sync + Send + 'static> = Box::new(stream);
+                    Either::B(future::ok((b, self)))
+                }
+            })
+            .and_then(|(stream, this)| this.connect_on(stream))
+    }
+
+    pub fn connect_on<S: AsyncRead + AsyncWrite>(
+        self,
+        stream: S,
+    ) -> impl Future<Item = Client<S>, Error = Error> {
+        let key = self.key.unwrap_or_else(|| rand::random());
+        tokio_io::io::write_all(
+            stream,
+            format!(
+                "GET {path} HTTP/1.1\r\n\
+                 Host: {host}\r\n\
+                 Upgrade: websocket\r\n\
+                 Connection: Upgrade\r\n\
+                 Sec-WebSocket-Key: {key}\r\n\
+                 Sec-WebSocket-Version: 13\r\n\
+                 \r\n",
+                path = self.url.path(),
+                host = self.url.domain().unwrap_or(""),
+                key = Base64Display::standard(&key)
+            ),
+        ).map_err(Into::into)
+            .and_then(move |(stream, _request)| {
+                #[allow(deprecated)]
+                let framed = stream.framed(UpgradeCodec);
+
+                framed.into_future().map_err(|(e, _framed)| e)
+            })
+            .and_then(move |(opt, framed)| {
+                opt.ok_or_else(|| "no HTTP Upgrade response".to_owned())?;
+
+                #[allow(deprecated)]
+                let framed = Framed::from_parts(framed.into_parts(), MessageCodec);
+
+                Ok(framed)
+            })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fmt;
+    use std::io::{self, Cursor, Read, Write};
+    use std::result;
+    use std::str;
+
+    use base64;
+    use futures::{Future, Poll};
+    use tokio_io::{AsyncRead, AsyncWrite};
+    use url::Url;
+
+    use super::ClientBuilder;
+
+    type Result<T> = result::Result<T, super::Error>;
+
+    pub struct ReadWritePair<R, W>(pub R, pub W);
+
+    impl<R: Read, W> Read for ReadWritePair<R, W> {
+        fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+            self.0.read(buf)
+        }
+
+        fn read_to_end(&mut self, buf: &mut Vec<u8>) -> io::Result<usize> {
+            self.0.read_to_end(buf)
+        }
+
+        fn read_to_string(&mut self, buf: &mut String) -> io::Result<usize> {
+            self.0.read_to_string(buf)
+        }
+
+        fn read_exact(&mut self, buf: &mut [u8]) -> io::Result<()> {
+            self.0.read_exact(buf)
+        }
+    }
+
+    impl<R, W: Write> Write for ReadWritePair<R, W> {
+        fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+            self.1.write(buf)
+        }
+
+        fn flush(&mut self) -> io::Result<()> {
+            self.1.flush()
+        }
+
+        fn write_all(&mut self, buf: &[u8]) -> io::Result<()> {
+            self.1.write_all(buf)
+        }
+
+        fn write_fmt(&mut self, fmt: fmt::Arguments) -> io::Result<()> {
+            self.1.write_fmt(fmt)
+        }
+    }
+
+    impl<R: AsyncRead, W> AsyncRead for ReadWritePair<R, W> {}
+
+    impl<R, W: AsyncWrite> AsyncWrite for ReadWritePair<R, W> {
+        fn shutdown(&mut self) -> Poll<(), io::Error> {
+            self.1.shutdown()
+        }
+    }
+
+    #[test]
+    fn can_connect_on() -> Result<()> {
+        let request = "GET /stream:8000 HTTP/1.1\r\n\
+                       Host: localhost\r\n\
+                       Upgrade: websocket\r\n\
+                       Connection: Upgrade\r\n\
+                       Sec-WebSocket-Key: x3JJHMbDL1EzLkh9GBhXDw==\r\n\
+                       Sec-WebSocket-Version: 13\r\n\
+                       \r\n";
+
+        let response = "HTTP/1.1 101 Switching Protocols\r\n\
+                        Upgrade: websocket\r\n\
+                        Connection: Upgrade\r\n\
+                        Sec-WebSocket-Accept: s3pPLMBiTxaQ9kYGzzhZRbK+xOo=\r\n\
+                        \r\n";
+
+        let mut input = Cursor::new(&response[..]);
+        let mut output = Cursor::new(Vec::new());
+        ClientBuilder::new(Url::parse("ws://localhost/stream:8000")?)
+            .key(&base64::decode(b"x3JJHMbDL1EzLkh9GBhXDw==")?)
+            .connect_on(ReadWritePair(&mut input, &mut output))
+            .wait()?;
+
+        assert_eq!(request, str::from_utf8(&output.into_inner())?);
+        Ok(())
+    }
+}
