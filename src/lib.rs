deeb04c14e03b85740f9b03c07d5f07d930dd463
Tim Robinson <tim.g.robinson@gmail.com>
2019-04-14 20:54:22 +0100

    Split out websocket-codec crate

--- src/lib.rs
+++ src/lib.rs
@@ -15,63 +15,19 @@
 //! This crate is fully conformant with the fuzzingserver module in the
 //! [Autobahn test suite](https://github.com/crossbario/autobahn-testsuite).
 
-extern crate base64;
-extern crate byteorder;
-extern crate bytes;
-extern crate futures;
-extern crate httparse;
-extern crate rand;
-extern crate sha1;
-extern crate take_mut;
-extern crate tokio_codec;
-extern crate tokio_io;
-extern crate tokio_tcp;
-extern crate url;
-
-#[cfg(test)]
-#[macro_use]
-extern crate quickcheck;
-
-#[cfg(all(feature = "nightly", test))]
-extern crate test;
-
-#[cfg(feature = "ssl-native-tls")]
-extern crate native_tls;
-
-#[cfg(feature = "ssl-native-tls")]
-extern crate tokio_tls;
-
-#[cfg(feature = "ssl-openssl")]
-extern crate openssl;
-
-#[cfg(feature = "ssl-openssl")]
-extern crate tokio_openssl;
-
 mod client;
-mod frame;
-mod mask;
-mod message;
-mod opcode;
 mod ssl;
 mod sync;
 mod upgrade;
 
 pub use crate::client::ClientBuilder;
-pub use crate::message::{Message, MessageCodec};
-pub use crate::opcode::Opcode;
 
-use std::error;
+pub use websocket_codec::{Error, Result, Message, MessageCodec, Opcode};
+
 use std::io::{Read, Write};
-use std::result;
 
 use tokio_codec::Framed;
 use tokio_io::{AsyncRead, AsyncWrite};
-
-/// Represents errors that can be exposed by this crate.
-pub type Error = Box<error::Error + 'static>;
-
-/// Represents results returned by the non-async functions in this crate.
-pub type Result<T> = result::Result<T, Error>;
 
 /// Used by [`AsyncClient`](type.AsyncClient.html) to represent types that are `AsyncRead` and `AsyncWrite`.
 pub trait AsyncNetworkStream: AsyncRead + AsyncWrite {}
