dd9f56b29a4e6048a06071ef6f88cea1bb7a4736
Tim Robinson <tim.g.robinson@gmail.com>
2020-07-11 08:55:47 +0100

    Safety: don't try to read from an unaligned `u32` slice

--- websocket-codec/src/mask.rs
+++ websocket-codec/src/mask.rs
@@ -1,8 +1,5 @@
 #![allow(clippy::new_without_default)]
 
-use std::mem;
-use std::slice;
-
 use rand;
 
 #[derive(Copy, Clone, Debug, PartialEq)]
@@ -27,39 +24,47 @@ impl From<Mask> for u32 {
 }
 
 /// Masks *by copying* data sent by a client, and unmasks data received by a server.
-pub fn mask_slice_copy(buf: &mut [u8], data: &[u8], mask: Mask) {
+pub fn mask_slice_copy(buf: &mut [u8], data: &[u8], Mask(mask): Mask) {
+    assert_eq!(buf.len(), data.len());
+
     let (buf1, buf2, buf3) = unsafe { buf.align_to_mut() };
     let (data1, data) = data.split_at(buf1.len());
-    let (data2, data3) = unsafe { unaligned(data) };
-    let Mask(mask) = mask;
-    let mask = mask_u8_copy(buf1, data1, mask);
-    mask_aligned_copy(buf2, data2, mask);
-    mask_u8_copy(buf3, data3, mask);
-}
-
-unsafe fn unaligned<T>(data: &[u8]) -> (&[T], &[u8]) {
-    let size = mem::size_of::<T>();
-    if size == 0 {
-        return (&[], data);
+    let (data_pre, data2, data3) = unsafe { data.align_to() };
+    if data_pre.is_empty() {
+        let mask = mask_u8_copy(buf1, data1, mask);
+        mask_aligned_copy(buf2, data2, mask);
+        mask_u8_copy(buf3, data3, mask);
+    } else {
+        let (data2, data3) = data.split_at(buf2.len() * 4);
+        let mask = mask_u8_copy(buf1, data1, mask);
+        mask_unaligned_copy(buf2, data2, mask);
+        mask_u8_copy(buf3, data3, mask);
     }
-
-    let len1 = data.len() / size;
-    (
-        slice::from_raw_parts(data.as_ptr() as *const T, len1),
-        &data[len1 * size..],
-    )
 }
 
 fn mask_aligned_copy(buf: &mut [u32], data: &[u32], mask: u32) {
-    debug_assert_eq!(buf.len(), data.len());
+    assert_eq!(buf.len(), data.len());
 
-    for (dest, &src) in buf.iter_mut().zip(data) {
+    for (dest, src) in buf.iter_mut().zip(data) {
+        *dest = src ^ mask;
+    }
+}
+
+fn mask_unaligned_copy(buf: &mut [u32], data: &[u8], mask: u32) {
+    let data = data.chunks_exact(4);
+    assert_eq!(data.len(), buf.len());
+    assert_eq!(data.remainder().len(), 0);
+
+    for (dest, src) in buf.iter_mut().zip(data) {
+        #[allow(clippy::cast_ptr_alignment)]
+        let src = unsafe { (src.as_ptr() as *const u32).read_unaligned() };
         *dest = src ^ mask;
     }
 }
 
 fn mask_u8_copy(buf: &mut [u8], data: &[u8], mut mask: u32) -> u32 {
-    debug_assert_eq!(buf.len(), data.len());
+    assert!(data.len() < 4);
+    assert_eq!(buf.len(), data.len());
 
     for (dest, &src) in buf.iter_mut().zip(data) {
         *dest = src ^ (mask as u8);
@@ -70,15 +75,16 @@ fn mask_u8_copy(buf: &mut [u8], data: &[u8], mut mask: u32) -> u32 {
 }
 
 /// Masks data sent by a client, and unmasks data received by a server.
-pub fn mask_slice(data: &mut [u8], mask: Mask) {
-    let Mask(mask) = mask;
-
+pub fn mask_slice(data: &mut [u8], Mask(mask): Mask) {
     let (data1, data2, data3) = unsafe { data.align_to_mut() };
     let mask = mask_u8_in_place(data1, mask);
     mask_aligned_in_place(data2, mask);
     mask_u8_in_place(data3, mask);
 }
+
 fn mask_u8_in_place(data: &mut [u8], mut mask: u32) -> u32 {
+    assert!(data.len() < 4);
+
     for b in data {
         *b ^= mask as u8;
         mask = mask.rotate_right(8);
@@ -86,6 +92,7 @@ fn mask_u8_in_place(data: &mut [u8], mut mask: u32) -> u32 {
 
     mask
 }
+
 fn mask_aligned_in_place(data: &mut [u32], mask: u32) {
     for n in data {
         *n ^= mask;
@@ -94,8 +101,6 @@ fn mask_aligned_in_place(data: &mut [u32], mask: u32) {
 
 #[cfg(test)]
 mod tests {
-    use std::mem;
-
     use assert_allocations::assert_allocated_bytes;
     use bytes::{BufMut, Bytes, BytesMut};
 
@@ -125,7 +130,7 @@ mod tests {
 
     #[test]
     fn can_mask() {
-        let mask = Mask::from(unsafe { mem::transmute::<[u8; 4], u32>([0xff, 0x00, 0x00, 0x01]) });
+        let mask = Mask::from(0xff000001u32.to_be());
         let orig_data = Bytes::from_static(DATA);
 
         let mut data = BytesMut::with_capacity(orig_data.len());
