ae06a70554a00e3e1ed304f11f01e362b2e95fdb
Jens Reidel <adrian@travitia.xyz>
2021-08-09 16:50:03 +0200

    add rustls support

--- websocket-lite/src/ssl.rs
+++ websocket-lite/src/ssl.rs
@@ -65,4 +65,58 @@ mod inner {
     }
 }
 
+#[cfg(any(feature = "ssl-rustls-webpki-roots", feature = "ssl-rustls-native-roots"))]
+mod inner {
+    use std::fmt::Debug;
+    use std::io::{Read, Write};
+    use std::sync::Arc;
+
+    use tokio::io::{AsyncRead, AsyncWrite};
+    use tokio_rustls::{rustls, webpki::DNSNameRef, TlsConnector};
+
+    use crate::Result;
+
+    #[cfg(feature = "ssl-rustls-webpki-roots")]
+    fn get_client_config() -> Arc<rustls::ClientConfig> {
+        let mut config = rustls::ClientConfig::new();
+        config
+            .root_store
+            .add_server_trust_anchors(&webpki_roots::TLS_SERVER_ROOTS);
+        Arc::new(config)
+    }
+
+    #[cfg(feature = "ssl-rustls-native-roots")]
+    fn get_client_config() -> Arc<rustls::ClientConfig> {
+        let mut config = rustls::ClientConfig::new();
+        config.root_store = match rustls_native_certs::load_native_certs() {
+            Ok(store) | Err((Some(store), _)) => store,
+            Err((None, err)) => Err(err).expect("cannot access native cert store"),
+        };
+        if config.root_store.is_empty() {
+            panic!("no CA certificates found");
+        }
+        Arc::new(config)
+    }
+
+    pub async fn async_wrap<S: AsyncRead + AsyncWrite + Unpin>(
+        domain: &str,
+        stream: S,
+    ) -> Result<tokio_rustls::client::TlsStream<S>> {
+        let connector = TlsConnector::from(get_client_config());
+        let tls_stream = connector
+            .connect(DNSNameRef::try_from_ascii_str(domain)?, stream)
+            .await?;
+
+        Ok(tls_stream)
+    }
+
+    pub fn wrap<S: Read + Write + Debug + 'static>(
+        domain: &str,
+        stream: S,
+    ) -> Result<rustls::StreamOwned<rustls::ClientSession, S>> {
+        let session = rustls::ClientSession::new(&get_client_config(), DNSNameRef::try_from_ascii_str(domain)?);
+        Ok(rustls::StreamOwned::new(session, stream))
+    }
+}
+
 pub use self::inner::*;
